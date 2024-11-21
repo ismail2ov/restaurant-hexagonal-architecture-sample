7bac222fe16f1fc6ce452b9471fc113e75be3ab1
Ismail Ahmedov <ismail2ov@gmail.com>
2024-11-21 01:23:07 +0100

    Add project files

---
+++ src/test/java/com/github/ismail2ov/restaurant/RestaurantApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.ismail2ov.restaurant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestaurantApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
